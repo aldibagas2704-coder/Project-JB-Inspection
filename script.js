f7407e02dbc994766668ca9ed62eee9e4eca6eee
aldibagas2704-coder <aldibagas2704@gmail.com>
2026-05-18 00:20:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,62 @@
-// ==========================
+// ===============================
+        imageBase64 = await new Promise((resolve) => {
 
-        file:
-          await new Promise(resolve => {
+          const reader = new FileReader();
 
-            if(!file){
+          reader.onload = function (event) {
+            resolve(event.target.result);
+          };
 
-              resolve("");
-              return;
+          reader.readAsDataURL(fileInput.files[0]);
 
-            }
+        });
+      }
 
-            const reader =
-              new FileReader();
-
-            reader.onload = e =>
-              resolve(e.target.result);
-
-            reader.readAsDataURL(file);
-
-          })
-
+      items.push({
+        description: inputs[0]?.value || '',
+        condition: inputs[1]?.value || '',
+        image: imageBase64,
+        partNumber: inputs[3]?.value || '',
+        namaBarang: inputs[4]?.value || '',
+        qty: inputs[5]?.value || '',
+        satuan: inputs[6]?.value || '',
+        componentGroup: inputs[7]?.value || '',
+        subComponent: inputs[8]?.value || '',
+        masukFPB: inputs[9]?.checked || false
       });
-
     }
 
     const payload = {
+      date: document.getElementById('Date').value,
+      site: document.querySelector('input[name="site"]').value,
+      codeUnit: document.querySelector('input[name="codeUnit"]').value,
+      hourMeter: document.querySelector('input[name="hourMeter"]').value,
+      inspectedBy: document.querySelector('input[name="inspectedBy"]').value,
+      priority: document.querySelector('input[name="priority"]').value,
+      items: items
+    };
 
-      action:"submitForm",
+    console.log('DATA FORM :', payload);
 
-      date:
-        document
-        .getElementById("Date")
-        ?.value || "",
+    output.classList.remove('d-none');
+    output.innerHTML = '✅ Data inspection berhasil diproses';
 
-      site:
-        document
-        .querySelector(
-          'input[name="site"]'
-        )?.value || "",
+    form.reset();
 
-      codeUnit:
-        document
-        .querySelector(
-          'input[name="codeUnit"]'
-        )?.value || "",
+    tableBody.innerHTML = '';
 
-      hourMeter:
-        document
-        .querySelector(
-          'input[name="hourMeter"]'
-        )?.value || "",
+    createRow('inspection');
 
-      inspectedBy:
-        document
-        .querySelector(
-      
+  } catch (error) {
+
+    console.error(error);
+
+    alert('Terjadi error pada sistem');
+
+  } finally {
+
+    overlay.classList.add('d-none');
+
+  }
+
+});
