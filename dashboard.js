3a2a082d2e88a761aa289d33b5c8fd764b622411
aldibagas2704-coder <aldibagas2704@gmail.com>
2026-06-20 21:46:58 +0700

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,30 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const buttons =
-    document.querySelectorAll(".sidebar button");
-
-  const iframe =
-    document.getElementById("iframe-content");
-
-  // =========================
-  // SIDEBAR CLICK
-  // =========================
+  const buttons = document.querySelectorAll(".nav-btn");
+  const iframe  = document.getElementById("iframe-content");
 
   buttons.forEach(btn => {
-
     btn.addEventListener("click", () => {
-
-      const page =
-        btn.dataset.page;
-
-      if(page){
-
+      const page = btn.dataset.page;
+      if (page) {
         iframe.src = page;
-
+        buttons.forEach(b => b.classList.remove("active"));
+        btn.classList.add("active");
       }
-
     });
-
   });
 
 });
